b878f83c60847b8cd76b1ea1be716ecac137a9cb
Administrator <Administrator@CHAUHAN>
2022-03-08 19:41:08 +0530

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Enter the Number");
+            i = Convert.ToInt32(Console.ReadLine());
+            if(i % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+                Console.ReadLine();
+            }
+        }
+    }
+}
